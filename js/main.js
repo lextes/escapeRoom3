7c5b5bf086007297fce9ff125e97dce562bd9867
lextes <forza1708@gmail.com>
2019-08-14 15:20:50 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var popupDiv = document.getElementById("popup_bg");
+
+function openPopupMenu(){
+	popupDiv.style.display = "block";
+}
+
+function closePopupMenu(){
+	popupDiv.style.display = "none";
+}
